4285602fc39acc59d4bb327b94e01e132e0e6f5e
Vedran Petrac <vedran.petrac18@gmail.com>
2020-04-26 18:48:21 +0200

    Version 1.0

---
+++ GameOfLife/game_of_life.cpp
@@ -0,0 +1,84 @@
+#include "game_of_life.h"
+
+
+
+bool game_of_life::rand50()
+{
+	return rand() & 1;
+}
+
+bool game_of_life::slucajna_vrijednost()
+{
+
+	return rand50() & rand50();
+}
+
+int game_of_life::brojSusjeda(int l, int k)
+{
+	counter = 0;
+
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) {
+			counter += _generacija[l + i][k + j];
+		}
+	}
+	counter -= _generacija[l][k];
+	return counter;
+}
+
+
+
+game_of_life::game_of_life()
+{
+	for (unsigned i = 0; i < REDAKA; i++)
+	{
+		for (unsigned j = 0; j < STUPACA; j++)
+		{
+			_generacija[i][j] = slucajna_vrijednost();
+		}
+	}
+}
+
+void game_of_life::sljedeca_generacija()
+{
+	
+
+	for (unsigned i = 0; i < REDAKA; i++)
+	{
+		for (unsigned j = 0; j < STUPACA; j++)
+		{
+			if ((_generacija[i][j] == 1) && (brojSusjeda(i, j) < 2))_sljedeca_generacija[i][j] = 0;
+
+			else if ((_generacija[i][j] == 1) && (brojSusjeda(i, j) > 3))_sljedeca_generacija[i][j] = 0;
+
+			else if ((_generacija[i][j] == 0) && (brojSusjeda(i, j) == 3))_sljedeca_generacija[i][j] = 1;
+
+			else _sljedeca_generacija[i][j] = _generacija[i][j];
+			
+		}
+	}
+
+	for (unsigned i = 0; i < REDAKA; i++)
+	{
+		for (unsigned j = 0; j < STUPACA; j++)
+		{
+			_generacija[i][j] = _sljedeca_generacija[i][j];
+		}
+	}
+
+	
+
+}
+
+void game_of_life::iscrtaj()
+{
+	for (unsigned i = 0; i < REDAKA; i++)
+	{
+		for (unsigned j = 0; j < STUPACA; j++)
+		{
+			if (_generacija[i][j] == 1)cout << "*";
+			else cout << "_";
+		}
+		cout << endl;
+	}
+}
---
+++ GameOfLife/game_of_life.h
@@ -0,0 +1,28 @@
+#ifndef _GAME_OF_LIFE_H_
+#define _GAME_OF_LIFE_H_
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+class game_of_life {
+private:
+	bool rand50();
+	unsigned int counter;
+	static const unsigned int STUPACA = 40;
+	static const unsigned int REDAKA = 20;
+	bool _generacija[REDAKA][STUPACA];
+	bool _sljedeca_generacija[REDAKA][STUPACA];
+	bool slucajna_vrijednost();
+	int brojSusjeda(int i,int j);
+
+public:
+	game_of_life();
+	void sljedeca_generacija();
+	void iscrtaj();
+};
+
+#endif
